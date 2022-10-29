41ac9438d6b64f028e648d72e700b5f5a318cafe
shiro4710 <54439127+shiro4710@users.noreply.github.com>
2022-10-30 00:29:29 +0900

    update: can fusion

--- unity/Assets/Scripts/MainController.cs
+++ unity/Assets/Scripts/MainController.cs
@@ -57,6 +57,19 @@ public class MainController : MonoBehaviour
     private float unityChanDisTime = 0;
     private const float UNITY_CHAN_DIS_TIME = 1f;
 
+    [SerializeField]
+    private float fusionLRDist = 6f;
+    [SerializeField]
+    private float fusionUpperDist = 2f;
+
+    [SerializeField]
+    private int fusionStatus = 0;
+    private bool reftFusion = false;
+
+    private int lastFusinoStatus = 0;
+    private const float DIS_FUSION_TIME = 1.5f;
+    private float disFusionTime = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -91,26 +104,73 @@ public class MainController : MonoBehaviour
         {
             kamehame_beam.SetActive(false);
             kamehame_charge.SetActive(false);
+            beamMusic.SetActive(false);
+            chargeMusic.SetActive(false);
             chargeTime = 0;
+        }
+        float hX = (markerPos[9].x + markerPos[10].x) / 2;
+        float sX = (markerPos[5].x + markerPos[6].x) / 2;
+        float hY = (markerPos[9].y + markerPos[10].y) / 2;
+        float sY = (markerPos[5].y + markerPos[6].y) / 2;
+        // フュージョン用
+        switch (fusionStatus)
+        {
+            case 0:
+                if (Mathf.Abs(sX - hX) >= fusionLRDist && sY + fusionUpperDist >= hY)
+                {
+                    fusionStatus++;
+                    reftFusion = sX > hX;
+                }
+                break;
+            case 1:
+                if(sY + fusionUpperDist <= hY)
+                {
+                    fusionStatus++;
+                }
+                break;
+            case 2:
+                if (Mathf.Abs(sX - hX) >= fusionLRDist && sY + fusionUpperDist >= hY)
+                {
+                    if ((reftFusion && sX < hX) || ((!reftFusion) && sX > hX))
+                    {
+                        fusionStatus++;
+                    }
+                }
+                break;
+            case 3:
+                if (Mathf.Abs(sX - hX) >= fusionLRDist && sY + fusionUpperDist >= hY)
+                {
+                    if ((reftFusion && sX > hX) || ((!reftFusion) && sX < hX))
+                    {
+                        fusionStatus = 0;
+                        unityChan.SetActive(!unityChan.activeSelf);
+                    }
+                }
+                break;
+        }
+        disFusionTime += Time.deltaTime;
+        if(lastFusinoStatus != fusionStatus)
+        {
+            lastFusinoStatus = fusionStatus;
+            disFusionTime = 0;
+        }
+        if(disFusionTime >= DIS_CHARGE_TIME && !unityChan.activeSelf)
+        {
+            fusionStatus = 0;
         }
         // 左右の距離確認
         if (markerPos[9] != Vector3.zero)
         {
             if ((kamehame_beam.activeSelf ? releaseTriggerDist : triggerDist) >= Vector3.Distance(markerPos[9], markerPos[10]))
             {
+                kamehame_charge.SetActive(true);
                 kamehame_charge.transform.localScale = chargeSize;
+                kamehame_charge.transform.position = new Vector3((markerPos[9].x + markerPos[10].x) / 2, (markerPos[9].y + markerPos[10].y) / 2, 0);
                 disChargeTime = 0;
                 chargeTime += Time.deltaTime;
                 if (chargeTime >= CHARGE_TIME)
                 {
-                    // 変身処理
-                    if(((markerPos[5].y + markerPos[6].y) / 2) -((markerPos[9].y + markerPos[10].y) / 2) >= tfDist)
-                    {
-                        unityChan.SetActive(!unityChan.activeSelf);
-                    }
                     // 正規の処理
-                    float hX = (markerPos[9].x + markerPos[10].x) / 2;
-                    float sX = (markerPos[5].x + markerPos[6].x) / 2;
                     if (releaseDist <= Mathf.Abs(hX - sX))
                     {
                         kamehame_beam.SetActive(true);
